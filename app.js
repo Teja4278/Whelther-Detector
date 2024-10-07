cac1767c84b31559ed9caf7fe52560a7a1318077
Teja4278 <gundasaiteja999@gmail.com>
2024-10-07 21:25:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+let city=document.getElementById('search-bar').value;
+let api=`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=EJ6UBL2JEQGYB3AA4ENASN62J&contentType=json`
+ 
